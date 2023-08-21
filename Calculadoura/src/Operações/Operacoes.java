8dd475157b88c8dcaa654d08de51af4048a9f002
Rayan <141056772+Rayan757575@users.noreply.github.com>
2023-08-21 10:40:41 -0300

    Add files via upload

---
+++ Calculadoura/src/Operações/Operacoes.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operações;
+
+/**
+ *
+ * @author 17500059647
+ */
+public class Operacoes {
+
+    public double divisao(double a, double b) {
+        return a / b;
+    }
+
+    public double multiplicacao(double a, double b) {
+        return a * b;
+    }
+
+    public double soma(double a, double b) {
+        return a + b;
+    }
+
+    public double subtracao(double a, double b) {
+        return a - b;
+    }
+
+    public double restoDivisao(double divisor, double dividendo) {
+        double resto = divisor % dividendo;
+        return resto;
+    }
+
+    public int proximonumero(int numero) {
+        return numero++;
+    }
+
+    public int anteriornumero(int numero) {
+        return numero --;
+    }
+    public double raiz(double numero) {
+        return  Math.sqrt(numero);
+    }
+    
+}
